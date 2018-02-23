133f381da358c32bd8f4588d79e82b1f137db1c9
David Whitehead <david__whitehead2@hotmail.com>
2018-02-23 10:54:58 +0000

    updated modframes front end packages and added a vue.js example.

--- projects/ASP.NET Core 2.0 - Full Framework/modules/examples/mvc/js/script.js
+++ projects/ASP.NET Core 2.0 - Full Framework/modules/examples/mvc/js/script.js
@@ -1,1 +1,18 @@
-﻿
+﻿// main vuejs app component
+var app = new Vue({
+    el: '#app',
+    data: {
+        textInput: null,
+        validMessage: 'you have entered some text!',
+        invalidMessage: 'Please enter some text.'
+    },
+    computed: {
+        //Used to calculate a true or false on showing valid or invalid classes
+        "IsValid": function () {
+            return (this.textInput != null && this.textInput != '');
+        },
+        "IsInvalid": function () {
+            return (this.textInput == '');
+        },
+    }
+})
---
+++ projects/ASP.NET Core 2.0 - NET Core/wwwroot/components/modules/Examples/MVC/js/min.js
@@ -0,0 +1,1 @@
+var app=new Vue({el:"#app",data:{textInput:null,validMessage:"you have entered some text!",invalidMessage:"Please enter some text."},computed:{IsValid:function(){return null!=this.textInput&&""!=this.textInput},IsInvalid:function(){return""==this.textInput}}});
